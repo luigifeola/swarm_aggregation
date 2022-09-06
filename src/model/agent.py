Solving BUG, now robots turn left and right. Metric updated, now the gradient is evaluated continuously and no qauntized. Some other fix

src/model/agent.py
@@ -39,6 +39,7 @@ class Agent:
         self.max_speed = speed
         self._radius = radius
         self.orientation = random.random() * 360  # 360 degree angle
+        self.turn_angle = 0
 
         self.bool_noise = bool_noise
         self.noise_mu = random.gauss(noise_mu, noise_musd)
@@ -67,6 +68,7 @@ class Agent:
         return f"ID: {self.id}\n" \
                f"drift: {round(self.noise_mu, 5)}\n" \
                f"position: {np.round(self.pos, 2)}\n" \
+               f"turning angle: {np.round(self.turn_angle, 2)}\n" \
                f"angle: {np.round(self.orientation, 2)}\n" \
                f"dr: {self.behavior.get_dr()}\n" \
                f"rho: {np.round(self.behavior.get_rw_factors()[0], 2)}\n" \
@@ -140,8 +142,12 @@ class Agent:
         angle = 0
         if self.levy_counter <= 1:
             angle = math.fabs(rw.wrapped_cauchy_ppf(self.crw_factor))
+            if random.randint(0, 1):
+                angle = -1.0 * angle
         self.update_levy_counter()
-        return angle
+        self.turn_angle = angle
+
+        return self.turn_angle
 
 
     def reset_levy_counter(self):

src/model/environment.py
@@ -111,15 +111,17 @@ class Environment:
             self.population.append(robot)
 
 
-    def quantize_val(self, val):
+    def pixel_filter(self, val, quantize_pixel=False):
         if val < 0:
             val = 0
         if val > 255:
             val = 255
         index = bisect(self.perceptible_thresholds[:-1], val / 255)
-        new_val = 255 * self.perceptible_gradient[index - 1]
+        if quantize_pixel:
+            quantized_val = 255 * self.perceptible_gradient[index - 1]
+            return quantized_val
 
-        return new_val
+        return val
 
     @staticmethod
     def quadratic_diffusion(k_val, distance):
@@ -168,11 +170,17 @@ class Environment:
                     print('Wrong diffusion type!!!')
                     exit(-1)
 
-                gray = self.quantize_val(gray)
+                gray = self.pixel_filter(gray, False)
                 background[x, y] = gray
                 background[x+1, y] = gray
                 background[x, y+1] = gray
                 background[x+1, y+1] = gray
+
+        # print('background.shape: ', background.shape)
+        # print('background.shape: ', background.size)
+        # print('pixels val sum / num of pixels / 255: ', np.sum(background)/background.size/255)
+        # linear 2 ---> .7366902396514161
+        # quadratic 3 ---> .7684166448801742
 
         return background
 

src/rw_gradient_follower.py
@@ -8,15 +8,15 @@ import numpy as np
 import shutil
 
 
-# to run the best result from irace with 3 quantization bits:
-# 6515485 1 1 /home/luigi/swarm_aggregation/config/config_batch.txt 3 --a0 1.9689 --r0 0.606 --st0 1 --a1 1.3391 --r1 0.3714 --st1 389 --a2 1.2121 --r2 0.8337 --st2 948
-## 6515485 1 1 /home/luigi/swarm_aggregation/config/config_batch.txt 4 --a0 1.9353 --r0 0.1620 --st0 503 --a1 1.966 --r1 0.27 --st1 762 --a2 1.218 --r2 0.3619 --st2 135 --a3 1.2229 --r3 0.8927 --st3 352
-
-# alpha0   rho0 str_steps0 alpha1   rho1 str_steps1 alpha2   rho2 str_steps2 alpha3   rho3 str_steps3
-# 1.9353 0.1620        503 1.9660 0.0270        762 1.2184 0.3619        135 1.2229 0.8927        352
+# linear 3 2k steps
+# 8943 1 8943 /home/luigi/swarm_aggregation/config/fixed_lambda_quadratic.txt 3 --std 13 --a0 1.52 --r0 0.91 --a1 1.74 --r1 0.48 --a2 1.98 --r2 0.68 --a3 1.47 --r3 0.36 --a4 1.61 --r4 0.35 --a5 1.94 --r5 0.18 --a6 1.7 --r6 0.44 --a7 1.6 --r7 0.97 --a8 1.22 --r8 0.13
 
 
+# linear 3 10k steps
+# 15562 1 15562 /home/luigi/swarm_aggregation/config/fixed_lambda_linear.txt 3 --std 6 --a0 1.61 --r0 0.93 --a1 1.85 --r1 0.88 --a2 1.99 --r2 0.3 --a3 1.3 --r3 0.15 --a4 1.93 --r4 0.63 --a5 1.85 --r5 0.01 --a6 1.79 --r6 0.77 --a7 1.42 --r7 0.37 --a8 1.2 --r8 0.03
 home_path = os.path.expanduser('~')
+
+# TODO: pass the gradient_memory flag from the config
 gradient_memory = True
 
 
@@ -30,15 +30,21 @@ def main():
                            config.parameters["WIDTH"],
                            config.parameters["HEIGHT"],
                            config.parameters["FPS"])
+
+        overall_gradient = main_controller.get_overall_gradient() / \
+                            config.parameters['NB_ROBOTS'] / \
+                            config.parameters['SIMULATION_STEPS']
+        print(f"Overall gradient {overall_gradient}")
     else:
-        run(config)                 # this is for single run
+        run(config)  # this is for single run
         os.remove(config_file)
 
 
 def generate_config_file(list_args):
     # print('list_args: ', list_args)
     config_path = list_args[3]
-    irace_config = home_path+"/swarm_aggregation/config/irace/irace_config"+list_args[0]+'_'+list_args[1]+'_'+list_args[2]+".txt"
+    irace_config = home_path + "/swarm_aggregation/config/irace/irace_config" + list_args[0] + '_' + list_args[
+        1] + '_' + list_args[2] + ".txt"
     # print('list_args: ', list_args)
     shutil.copyfile(config_path, irace_config)
     # print(list_args[2:])
@@ -52,9 +58,9 @@ def generate_config_file(list_args):
         exit(1)
 
     with open(irace_config, "a") as file:
-        q_bits_str = 'QUANTIZATION_BITS='+q_bits
+        q_bits_str = 'QUANTIZATION_BITS=' + q_bits
         std_str = 'STD_MOTION_STEPS='
-        _values = int(q_bits)*int(q_bits) if gradient_memory else int(q_bits)
+        _values = int(q_bits) * int(q_bits) if gradient_memory else int(q_bits)
         for i in range(_values):
             std_str += std + ','
 
@@ -62,7 +68,7 @@ def generate_config_file(list_args):
         file.write(std_str[:-1] + '\n')
 
     reshape_args = np.array(list_args[7:]).reshape(-1, 2)
-    num_param = reshape_args.shape[0]//_values
+    num_param = reshape_args.shape[0] // _values
     for i in range(num_param):
         idxs = np.arange(i, reshape_args.shape[0], num_param)
         values = np.take(reshape_args[:, 1], idxs)
@@ -74,8 +80,8 @@ def generate_config_file(list_args):
         if 'st' in reshape_args[i, 0]:
             param = 'STD_MOTION_STEPS='
 
-        array_str = param+str(values).replace(' [', '').replace('[', '').replace(']', '').replace(' ', ',')\
-                                                                                         .replace("'", "")
+        array_str = param + str(values).replace(' [', '').replace('[', '').replace(']', '').replace(' ', ',') \
+            .replace("'", "")
 
         with open(irace_config, "a") as file:
             file.write(array_str + '\n')
@@ -118,7 +124,10 @@ def run(config):
         file.write(f"{argv[1:]}: {controller.environment.center_gradient}\n")
     # print(f"Overall gradient for {config.parameters['NB_ROBOTS']} robots: "
     #       f"{controller.get_overall_gradient()}")
-    print(f"{controller.get_overall_gradient()}")
+    overall_gradient = controller.get_overall_gradient() / \
+                       config.parameters['NB_ROBOTS'] / \
+                       config.parameters['SIMULATION_STEPS']
+    print(f"{overall_gradient}")
 
 
 if __name__ == '__main__':